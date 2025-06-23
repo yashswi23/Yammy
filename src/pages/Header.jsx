440ca12ad715240f48f7fc227b8332cb83d8c6be
yashswi23 <yashswi2314@gmail.com>
2025-06-23 23:26:24 +0530

    Adding Search Functionality

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import "../styles/Header.css";
+import { useState } from 'react';
 
 const Header = () => {
+  let btnn="login";
+  
+   const [loginbtn,setlogin]= useState("login");
+  
   return (
+   
     <div className="header">
         <div className="logo">
             <img className='logo-img' src="https://cdn.dribbble.com/userupload/16229601/file/original-6b1a728052819b02f17b15b275d9e233.jpg?resize=400x0" />
@@ -12,7 +18,9 @@ const Header = () => {
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact Us</li>
-                <li>Login</li>
+                <button className='btn' onClick={()=>{
+                  setlogin("Logout"); console.log("Dyamic Button Now!");
+                }}>{loginbtn}</button>
             </ul>
         </div>
     </div>
--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Restaurant from '../components/Restaurant/Restaurant';
 import "../styles/Res.css";
-import { resLists } from '../components/Restaurant/Restaurant';
+import resLists  from "../utils/mockData";
+import { useState } from 'react';
 
 const Restaurants = () => {
+  const [mock, setMockData] = useState(resLists);
+  const [setf, setFiltered]  = useState(resLists);
+  const [searchtext, setSearchText] = useState("");
+useEffect(()=>{
+    setMockData(resLists);
+    console.log("mock data");
+  },[]);
+console.log("Body rendered");
   return (
-    <div className='res-lists'>
-     {resLists.map((rest)=>( <Restaurant key={rest._id} resData={rest}/>))}
+    <>
+    <div className='search'>
+      <input type='text' className='sea' onChange={(e)=>{
+        setSearchText(e.target.value);
+      }}></input>
+      <button className='sch' onClick={()=>{
+        const filteredRes = mock.filter((res) =>
+  res.name.toLowerCase().includes(searchtext.toLowerCase())
+);
+        setFiltered(filteredRes);
+      }}>Search
+      </button>
     </div>
+    <div className='btn'>
+      <button className='filter-btn' onClick={()=>{
+        const filtered = resLists.filter((res)=> res.rating> 4.3);
+        setFiltered(filtered);
+      }}>Top Resturants</button>
+    </div>
+    <div className='res-lists'>
+     {setf.map((rest)=>( <Restaurant key={rest._id} resData={rest}/>))}
+    </div>
+    </>
   )
 }
 
